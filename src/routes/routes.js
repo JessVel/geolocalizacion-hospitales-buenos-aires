9da168e48bb9e66903b6b9defeeec6cdd9169dac
Epilef Rodriguez <epilef.rodriguez@gmail.com>
2020-08-10 08:15:30 -0400

    env variables, google maps y leafletjs

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,9 +1,42 @@
 const router = require("express").Router();
 const axios = require("axios");
+require('dotenv').config()
 
 //ruta de mapa con google API
 router.get("/mapa-google", (req, res) => {
-  res.render("google-map.ejs");
+  res.render("google-map.ejs", {
+    apiKey: process.env.GOOGLE_MAPS_API_KEY
+  });
+});
+
+//ruta del mapa en leaflet con los marcadores de los hospitales
+router.get('/leaflet', async (req,res) =>{
+  await axios
+    .get(
+      "http://cdn.buenosaires.gob.ar/datosabiertos/datasets/hospitales/hospitales.geojson"
+    )
+    .then((data) => {
+      let objetos = data.data.features;
+      let hospitales = [];
+
+      objetos.forEach((objeto) => {
+        let locationData = {
+          position: {
+            id: objeto.properties.ID,
+            name: objeto.properties.NOMBRE,
+            lat: objeto.geometry.coordinates[1],
+            lng: objeto.geometry.coordinates[0],
+          },
+        };
+        hospitales.push(locationData);
+      });
+      res.render('leaflet.ejs', {
+        hospitales: hospitales
+      })
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
 //ruta de json de datos de hospitales de buenos aires
@@ -34,10 +67,5 @@ router.get("/hospitales", async (req, res) => {
       console.log(err);
     });
 });
-
-//ruta del mapa en leaflet con los marcadores de los hospitales
-router.get('/hospitales/mapa', (req,res) =>{
-  res.render('leaflet.ejs')
-})
 
 module.exports = router;
